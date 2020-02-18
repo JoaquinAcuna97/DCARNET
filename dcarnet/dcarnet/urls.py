ef190ebc460afb0e8ba7b11f33a6031ae9cd84f2
joaquinacuna <jacuna@asapdevelopers.com>
2020-02-18 15:31:41 -0300

    add url patterns

--- dcarnet/dcarnet/urls.py
+++ dcarnet/dcarnet/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
+from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('carnets/', include('carnets.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
