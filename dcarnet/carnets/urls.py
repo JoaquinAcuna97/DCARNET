5b213e8f4faf6a7b5b060d3e423faebfb37eb5f2
mauricio <you@example.com>
2020-03-06 23:54:01 -0300

    se creo la create view de persona

--- dcarnet/carnets/urls.py
+++ dcarnet/carnets/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url, include
 from autenticacion import views as aviews
 from . import views
 
-
+app_name =  'carnets'
 urlpatterns = [
     url(r"^autenticacion/", include("autenticacion.urls")),
-    path("controlesmedicos/", views.controlesmedicos, name="Vercontroles"),
-    path("agregarcontrol/", views.agregarcontrol, name="Agregarcontrol"),
+    path("controles_medicos/", views.controlesmedicos, name="Ver_controles"),
+    path("agregar_control/", views.agregarcontrol, name="Agregar_control"),
+    path("Crear_Persona/",views.PersonaCreate.as_view(), name="Crear_Persona"),
 ]
