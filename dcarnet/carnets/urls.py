819f8c8702a23f5f33a1ff75e70cd1623ccdc021
mauricio <you@example.com>
2020-03-11 00:07:53 -0300

    Se creo detailview y createview de nino y controles_medicos. Se crearon urls de nino y control_medico. Se ordenaron algunos templates. ¡REVISAR!

--- dcarnet/carnets/urls.py
+++ dcarnet/carnets/urls.py
@@ -21,4 +21,16 @@ urlpatterns = [
         name="detail_familiar",
     ),
     path("Crear_Familiar/", views.FamiliarCreate.as_view(), name="crear_familiar"),
+    path(
+        "detail_Nino/<int:pk>/",
+        views.PerfilNinoView.as_view(),
+        name="detail_nino",
+    ),
+    path("Crear_Nino/", views.NinoCreate.as_view(), name="crear_nino"),
+    path(
+        "detail_Control_medico/<int:pk>/",
+        views.PerfilControl_medicoView.as_view(),
+        name="detail_control_medico",
+    ),
+    path("Crear_Control_medico/", views.Control_medicoCreate.as_view(), name="crear_control_medico"),
 ]
