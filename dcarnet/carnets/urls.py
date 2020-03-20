e4a35369fb517ad11cef7be49c9f184b69f8b1f4
joaquin <juaco1718@gmail.com>
2020-03-20 19:59:21 -0300

    se agregan las barras

--- dcarnet/carnets/urls.py
+++ dcarnet/carnets/urls.py
@@ -6,34 +6,34 @@ from . import views
 app_name = "carnets"
 
 urlpatterns = [
-    url(r"^autenticacion", include("autenticacion.urls")),
+    url(r"^autenticacion/", include("autenticacion.urls")),
     path(
         "detail_Medico/<int:pk>",
         views.PerfilMedicoView.as_view(),
         name="detail_medico",
     ),
-    path("Crear_Medico", views.MedicoCreate.as_view(), name="crear_medico"),
+    path("Crear_Medico/", views.MedicoCreate.as_view(), name="crear_medico"),
     path(
         "detail_Familiar/<int:pk>",
         views.PerfilFamiliarView.as_view(),
         name="detail_familiar",
     ),
-    path("Crear_Familiar", views.FamiliarCreate.as_view(), name="crear_familiar"),
+    path("Crear_Familiar/", views.FamiliarCreate.as_view(), name="crear_familiar"),
     path("detail_Nino/<int:pk>", views.PerfilNinoView.as_view(), name="detail_nino",),
-    path("list_nino", views.NinoListView.as_view(), name="nino_list"),
-    path("Crear_Nino", views.NinoCreate.as_view(), name="crear_nino"),
+    path("list_nino/", views.NinoListView.as_view(), name="nino_list"),
+    path("Crear_Nino/", views.NinoCreate.as_view(), name="crear_nino"),
     path(
         "detail_Control_medico/<int:pk>",
         views.Perfil_Control_medico_View.as_view(),
         name="detail_Control_medico",
     ),
     path(
-        "Crear_Control_medico",
+        "Crear_Control_medico/",
         views.Control_medicoCreate.as_view(),
         name="crear_control_medico",
     ),
     path(
-        "list_control_medico",
+        "list_control_medico/",
         views.Control_medico_List_View.as_view(),
         name="control_list",
     ),
